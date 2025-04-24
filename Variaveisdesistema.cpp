708b935d44d0d2b044a2de0fedbbea8dee8662a5
Nuklles <103897836+Nuklles@users.noreply.github.com>
2025-04-24 20:01:39 -0300

    Create Variaveisdesistema.cpp

---
+++ Variaveisdesistema.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+
+  int numero = 20; 
+  cout << numero << endl;
+
+  float valor = 2.5;
+  cout << valor << endl;
+
+  double real = 5.2e99;
+  cout << real << endl;
+
+  bool booleano;
+  booleano = true;
+  cout << booleano << endl; 
+
+  bool boolean;
+  boolean = false;
+  cout << boolean << endl; 
+
+  char letra = 'D';
+  cout << letra << endl;
+   
+  string nome = "Daniel";
+  cout << nome << endl;
+  
+   //Para o usuário imprimir algo
+   int idade;
+   cout << "Qual sua idade:" << endl;
+   cin >> idade; 
+   cout << "Sua idade é: " << idade << endl;
+
+  return 0;
+}
